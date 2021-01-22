4fe96bcfeeb8390334aef66f2d96a2036b298a72
Greg Myers <greg.myers@cern.ch>
2021-01-22 20:12:04 +0100

    initial commit

---
+++ Assets/Scenes/InitialStateA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialStateA : MonoBehaviour
+{
+    public GameObject ProtonBunchA;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.velocity = new Vector3(10, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scenes/InitialStateC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialStateC : MonoBehaviour
+{
+    public GameObject ProtonBunchC;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(-10, 0, 0);
+        }
+    }
+}
